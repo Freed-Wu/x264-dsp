2439d20e20a040617e946d74037309e9cfd8d48d
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-12-27 15:49:15 +0800

    :truck: Move assets/gel/ to targetConfigs/

--- scripts/generate-compile_commands.json-for-gel.sh
+++ scripts/generate-compile_commands.json-for-gel.sh
@@ -2,6 +2,6 @@
 set -e
 cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-mkdir -p assets/gel/build
-find "$PWD" -wholename "$PWD/assets/gel/*.gel" |
-  scripts/generate-compile_commands.json-for-gel.jq >assets/gel/build/compile_commands.json
+mkdir -p targetConfigs/gel/build
+find "$PWD" -wholename "$PWD/targetConfigs/gel/*.gel" |
+  scripts/generate-compile_commands.json-for-gel.jq >targetConfigs/gel/build/compile_commands.json
