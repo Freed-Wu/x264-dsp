f67d7320c49e05e4f1d586685f71246eea25fcdf
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-12-27 14:13:27 +0800

    :bento: Add assets/gel/davincihd1080p_arm.gel

--- scripts/burn.js
+++ scripts/burn.js
@@ -7,10 +7,14 @@ ds.setConfig("targetConfigs/TMS320DM6467.ccxml");
 var arm926 = ds.openSession(
   "Texas Instruments XDS100v3 USB Debug Probe/ARM926"
 );
-arm926.expression.evaluate('GEL_LoadGel("assets/gel/davincihd_arm.gel")');
+// /opt/ccstudio/ccs/ccs_base/emulation/boards/evmdm6467/gel/davincihd1080p_arm.gel
+// comment Setup_Pll0_990_MHz_OscIn()
+arm926.expression.evaluate('GEL_LoadGel("assets/gel/davincihd1080p_arm.gel")');
 arm926.target.connect();
+// after OnTargetConnect()
+arm926.expression.evaluate('DSP_Boot_from_DDR2()');
 var c64xp = ds.openSession("Texas Instruments XDS100v3 USB Debug Probe/C64XP");
-c64xp.expression.evaluate('GEL_LoadGel("assets/gel/davincihd_dsp.gel")');
+c64xp.expression.evaluate('GEL_LoadGel("/opt/ccstudio/ccs/ccs_base/emulation/boards/evmdm6467/gel/davincihd1080p_dsp.gel")');
 c64xp.target.connect();
 c64xp.memory.loadProgram("x264.out", ["x264.out"].concat(arguments));
 c64xp.clock.runBenchmark();
