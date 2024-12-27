d39a3b34bde5405de8706f0939a037307ae0b7e9
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-12-27 20:37:52 +0800

    :technologist: Update scripts/burn.js

--- scripts/burn.js
+++ scripts/burn.js
@@ -9,12 +9,29 @@ var arm926 = ds.openSession(
 );
 // /opt/ccstudio/ccs/ccs_base/emulation/boards/evmdm6467/gel/davincihd1080p_arm.gel
 // comment Setup_Pll0_990_MHz_OscIn()
-arm926.expression.evaluate('GEL_LoadGel("assets/gel/davincihd1080p_arm.gel")');
+arm926.expression.evaluate(
+  'GEL_LoadGel("targetConfigs/gel/davincihd1080p_arm.gel")'
+);
 arm926.target.connect();
 // after OnTargetConnect()
-arm926.expression.evaluate('DSP_Boot_from_DDR2()');
+arm926.expression.evaluate("DSP_Boot_from_DDR2()");
 var c64xp = ds.openSession("Texas Instruments XDS100v3 USB Debug Probe/C64XP");
-c64xp.expression.evaluate('GEL_LoadGel("/opt/ccstudio/ccs/ccs_base/emulation/boards/evmdm6467/gel/davincihd1080p_dsp.gel")');
+c64xp.expression.evaluate(
+  'GEL_LoadGel("/opt/ccstudio/ccs/ccs_base/emulation/boards/evmdm6467/gel/davincihd1080p_dsp.gel")'
+);
 c64xp.target.connect();
-c64xp.memory.loadProgram("x264.out", ["x264.out"].concat(arguments));
-c64xp.clock.runBenchmark();
+var bin = "build/x264.out";
+var args = [];
+for (i in arguments)
+  if (arguments[i] === "--") {
+    args = [bin].concat(arguments.slice(Number(i) + 1));
+    break;
+  }
+c64xp.memory.loadProgram(bin, args);
+c64xp.memory.verifyProgram(bin);
+if (arguments[0] && arguments[0] !== "--") {
+  var ddr2 = 0xa0000000;
+  arm926.memory.loadRaw(0, ddr2, arguments[0], 32, false); // DDR2 SDRAM
+  arm926.memory.verifyBinaryProgram(arguments[0], ddr2);
+}
+if (args.length) c64xp.clock.runBenchmark();
